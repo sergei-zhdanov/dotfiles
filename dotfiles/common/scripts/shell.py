f50873f5c0b6a83bba1fd18dd16c492083a9efbe
Sergei Zhdanov <szhdn@yandex.ru>
2020-05-01 15:42:07 +0300

    .

--- dotfiles/common/scripts/shell.py
+++ dotfiles/common/scripts/shell.py
@@ -1,1 +1,52 @@
-/usr/lib/python3.8/site-packages/i3pystatus/shell.py
+from i3pystatus import IntervalModule
+from i3pystatus.core.util import internet, require
+from i3pystatus.core.command import run_through_shell
+
+
+class Shell(IntervalModule):
+    """
+    Shows output of shell command
+
+    .. rubric:: Available formatters
+
+    * `{output}` — just the striped command output without newlines
+    """
+
+    color = "#FFFFFF"
+    error_color = "#FF0000"
+    ignore_empty_stdout = False
+
+    settings = (
+        ("command", "command to be executed"),
+        ("ignore_empty_stdout", "Let the block be empty"),
+        ("color", "standard color"),
+        ("error_color", "color to use when non zero exit code is returned"),
+        "format"
+    )
+
+    required = ("command",)
+    format = "{output}"
+
+    @require(internet)
+    def run(self):
+        retvalue, out, stderr = run_through_shell(self.command, enable_shell=True)
+
+        if retvalue != 0:
+            self.logger.error(stderr if stderr else "Unknown error")        
+        if out:
+            out = out.replace("\n", " ").strip()
+        elif stderr:
+            out = stderr
+
+        full_text = self.format.format(output=out).strip()
+        if not full_text and not self.ignore_empty_stdout:
+            full_text = "Command `%s` returned %d" % (self.command, retvalue)
+        self.output = {
+            "full_text": full_text,
+            "color": self.color if retvalue == 0 else self.error_color
+        }   
+
+        if out == '0':
+            self.output = None
+            return
+ 
